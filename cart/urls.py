9a12c5ef5d8a6cd8bb5864241a1103b827fa0625
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-03-21 13:02:49 +0000

    View Cart

--- cart/urls.py
+++ cart/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import url, include
-from .views import add_to_cart
+from .views import add_to_cart, view_cart
 
 urlpatterns = [
     url(r'^add/$', add_to_cart, name='add_to_cart'), 
+    url(r'^view/$', view_cart, name='view_cart'),
     ]
--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,35 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404
+from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
 from products.models import Product
+from decimal import Decimal
 
 # Create your views here.
 def add_to_cart(request):
     id = request.POST['id']
-    quantity = request.POST['quantity']
-    product = get_object_or_404(Product, pk=id)
-    return HttpResponse("You want to add {0} of {1}".format(quantity, product.name))
+    quantity = int(request.POST['quantity'])
+    cart = request.session.get('cart', {})
+    cart[id] = cart.get(id, 0) + quantity
+    request.session['cart'] = cart   
+    return redirect('home')
+    
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    
+    cart_items= []
+    total = 0
+    for item_id, item_quantity in cart.items():
+        this_product = get_object_or_404(Product, pk=item_id)
+        this_total = this_product.price * Decimal(item_quantity)
+        total += this_total
+        this_item = {
+            'image': this_product.image,
+            'name': this_product.name,
+            'quantity': item_quantity,
+            'price': this_product.price,
+            'total': this_total,
+        }
+        cart_items.append(this_item)
+    
+    args = {'cart_items': cart_items, 'total': total }
+    
+    return render(request, "cart/view_cart.html", args)
+    
