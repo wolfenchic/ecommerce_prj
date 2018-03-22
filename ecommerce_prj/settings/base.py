59ca257347c45b3e3e9ba99c6ed9ec9297b2467c
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-03-22 11:43:22 +0000

    changed debug

--- ecommerce_prj/settings/base.py
+++ ecommerce_prj/settings/base.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["ecommerce-app-wolfenchic.c9users.io", "wolfenchic-ecommerce.herokuapp.com"]
 
