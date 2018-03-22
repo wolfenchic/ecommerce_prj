2caa7178f1b77ca9196cf5d30660d375ca972502
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-03-22 15:50:15 +0000

    move cart to checkout

--- cart/views.py
+++ cart/views.py
@@ -1,43 +1,29 @@
-from django.shortcuts import render, HttpResponse, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from products.models import Product
 from decimal import Decimal
+from cart.utils import get_cart_items_and_total
 
-# Create your views here.
+def view_cart(request):
+    cart = request.session.get('cart', {})
+    context = get_cart_items_and_total(cart)
+    return render(request, "cart/view_cart.html", context)
+ 
+
 def add_to_cart(request):
     id = request.POST['id']
     quantity = int(request.POST['quantity'])
+
     cart = request.session.get('cart', {})
     cart[id] = cart.get(id, 0) + quantity
+    
     request.session['cart'] = cart   
-    return redirect('home')
-    
-def view_cart(request):
-    cart = request.session.get('cart', {})
 
-    cart_items= []
-    total = 0
-    for item_id, item_quantity in cart.items():
-        this_product = get_object_or_404(Product, pk=item_id)
-        this_total = this_product.price * Decimal(item_quantity)
-        total += this_total
-        this_item = {
-            'product_id': item_id,
-            'image': this_product.image,
-            'name': this_product.name,
-            'quantity': item_quantity,
-            'price': this_product.price,
-            'total': this_total,
-        }
-        cart_items.append(this_item)
-    
-    args = {'cart_items': cart_items, 'total': total }
-    
-    return render(request, "cart/view_cart.html", args)
-    
-    
+    return redirect('home')
+
+
+
 def delete_cart_item(request, id):
     cart = request.session.get('cart', {})
     del cart[id]
-    request.session['cart'] = cart    
-    return redirect("view_cart")
-    
+    request.session['cart'] = cart   
+    return redirect('view_cart')    
--- checkout/views.py
+++ checkout/views.py
@@ -1,7 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .forms import OrderForm, MakePaymentForm
-
+from products.models import Product
+from decimal import Decimal
+from cart.utils import get_cart_items_and_total
 
 # Create your views here.
 def checkout(request):
@@ -9,4 +11,11 @@ def checkout(request):
     order_form = OrderForm()
     payment_form = MakePaymentForm()
     
-    return render(request, "checkout/checkout.html",  {'order_form': order_form, 'payment_form': payment_form, 'publishable': 'whatever' })
+    context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': 'whatever' }
+    
+    cart = request.session.get('cart', {})
+    cart_items_and_total = get_cart_items_and_total(cart)
+    
+    context.update(cart_items_and_total)
+
+    return render(request, "checkout/checkout.html", context)
