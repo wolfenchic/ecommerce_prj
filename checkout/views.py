caf678698e4c2f7cdf666aa862b5e66c5d3b41a7
Nichola Wolfe <wolfe.nichola@gmail.com>
2018-03-22 16:17:06 +0000

    Order line items

--- checkout/views.py
+++ checkout/views.py
@@ -1,21 +1,45 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from .forms import OrderForm, MakePaymentForm
 from products.models import Product
 from decimal import Decimal
 from cart.utils import get_cart_items_and_total
+from django.utils import timezone
+from .models import OrderLineItem
 
 # Create your views here.
 def checkout(request):
-    
-    order_form = OrderForm()
-    payment_form = MakePaymentForm()
-    
-    context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': 'whatever' }
-    
-    cart = request.session.get('cart', {})
-    cart_items_and_total = get_cart_items_and_total(cart)
-    
-    context.update(cart_items_and_total)
+    if request.method=="POST":
+        # Save The Order
+        order_form = OrderForm(request.POST)
+        order = order_form.save(commit=False)
+        order.date = timezone.now()
+        order.save()
+        
+        # Save the Order Line Items
+        cart = request.session.get('cart', {})
+        for id, quantity in cart.items():
+            product = get_object_or_404(Product, pk=id)
+            order_line_item = OrderLineItem(
+                order = order,
+                product = product,
+                quantity = quantity
+                )
+            order_line_item.save()
+        
+        
+        # Charge the Card
+        
+        
+        #Clear the Cart
+        del request.session['cart']
+        return redirect("home")
+    else:
+        order_form = OrderForm()
+        payment_form = MakePaymentForm()
+        context = {'order_form': order_form, 'payment_form': payment_form, 'publishable': 'whatever' }
+        cart = request.session.get('cart', {})
+        cart_items_and_total = get_cart_items_and_total(cart)
+        context.update(cart_items_and_total)
 
     return render(request, "checkout/checkout.html", context)
